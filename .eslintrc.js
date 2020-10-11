ee4db28b7cd825c6016a2388588aad40c8cb4a8c
henek <eagluhih@gmail.com>
2020-10-11 22:42:58 +0500

    Добавлен eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        ecmaVersion: 8
+    },
+    plugins: ["gridsome"],
+    extends: ["plugin:gridsome/recommended", "plugin:vue/vue3-recommended"]
+}
